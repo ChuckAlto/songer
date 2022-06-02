e6ab7b407de120c6ee6082bb0f89802828cf84f1
Your Name <chuckalto@gmail.com>
2022-06-02 18:29:20 -0400

    initial commit

---
+++ src/test/java/com/chucksonger/songer/SongerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chucksonger.songer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SongerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

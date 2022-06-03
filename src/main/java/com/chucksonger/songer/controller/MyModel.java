fc0961791d4ef1d1dc5a941a69b74a9339e4b646
Your Name <chuckalto@gmail.com>
2022-06-02 20:27:41 -0400

    copied files over

---
+++ src/main/java/com/chucksonger/songer/controller/MyModel.java
@@ -0,0 +1,10 @@
+package com.chucksonger.songer.controller;
+
+public class MyModel {
+    public String title;
+    public String artist;
+    public int songCount;
+    public double albumLengthSeconds;
+
+
+}
---
+++ src/main/java/com/chucksonger/songer/controller/Songer.java
@@ -0,0 +1,19 @@
+package com.chucksonger.songer.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@RequestMapping("/")
+public class Songer {
+    @GetMapping("/")
+    public String getTest(){
+        return "songrSplash";
+    }
+
+
+
+}
